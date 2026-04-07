1f8f567fa1bcfd3c275a33102eac3bf8bc03ed3a
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-07 17:32:48 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <--- ESSA LINHA CONECTA O DESIGN AO SITE
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
