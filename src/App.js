51abc0f43466e02b596bcab421afb724b8781f16
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-07 12:13:15 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ListaLeads } from './components/ListaLeads';
 import React from 'react';
 import { Login } from './components/Login';
 import { Registro } from './components/Registro';
@@ -14,6 +15,7 @@ function App() {
       {/* Menu de Visualização Temporário para você testar */}
       <nav className="bg-white p-2 flex gap-4 text-[10px] border-b justify-center">
         <button onClick={() => setPagina('registro')}>1. Criar Conta (Admin)</button>
+        <button onClick={() => setPagina('lista')}>5. Ver Leads</button>
         <button onClick={() => setPagina('login')}>2. Login</button>
         <button onClick={() => setPagina('leads')}>3. Gestão de Leads</button>
         <button onClick={() => setPagina('admin')}>4. Painel do Dono</button>
@@ -24,6 +26,7 @@ function App() {
       {pagina === 'login' && <Login />}
       {pagina === 'leads' && <NovoLeadForm />}
       {pagina === 'admin' && <AdminLogs />}
+      {pagina === 'lista' && <ListaLeads />}
     </div>
   );
 }
