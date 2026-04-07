a0bd2f305018514a405d1b345c6f50d11a6cd6f9
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-07 18:04:34 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // <--- ESSA LINHA CONECTA O DESIGN AO SITE
 import App from './App';
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
