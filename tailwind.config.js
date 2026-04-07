e9b65e43667d39b0d0644e532de335f8662f2d8e
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-07 19:15:15 -0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
-// Configuração de Design do Klinni IA
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {
       colors: {
-        // As cores que você escolheu para o padrão Boutique
-        'ice-gray': '#F4F7F6',    // Fundo Relaxante
-        'soft-blue': '#7FA9D1',   // Botão Principal (Início do Degradê)
-        'deep-blue': '#9BB8CD',   // Botão Principal (Fim do Degradê)
-        'clinic-white': '#FFFFFF' // Cards e Superfícies
+        'ice-gray': '#F4F7F6',
+        'soft-blue': '#7FA9D1',
+        'deep-blue': '#9BB8CD',
+        'clinic-white': '#FFFFFF'
       },
       borderRadius: {
-        'clinic': '16px', // Bordas suaves que você solicitou
+        'clinic': '16px',
       },
       fontFamily: {
-        'inter': ['Inter', 'sans-serif'], // Fonte moderna e limpa
+        'inter': ['Inter', 'sans-serif'],
       }
     }
-  }
+  },
+  plugins: [],
 }
