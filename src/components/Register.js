f78712a5d267afd479f30314590f2794c0ec334d
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 18:02:32 -0300

    Register.js

---
+++ src/components/Register.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+import{auth}from'../firebase/config';
+import{createUserWithEmailAndPassword}from'firebase/auth';
+
+export const Register=()=>{
+const[email,setEmail]=useState('');
+const[password,setPassword]=useState('');
+const handleSubmit=async(e)=>{
+e.preventDefault();
+try{
+await createUserWithEmailAndPassword(auth,email,password);
+alert('Sucesso');
+}catch(err){
+alert(err.message);
+}};
+return(
+<div>
+<form onSubmit={handleSubmit}>
+<input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" required/>
+<input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Senha" required/>
+<button type="submit">Acessar</button>
+</form>
+</div>
+);};
