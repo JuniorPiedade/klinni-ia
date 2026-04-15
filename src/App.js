5f97e3d245ca75cbc714ee6ef4d82d82698d1e55
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 17:54:08 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { AuthProvider } from './AuthContext';
 import { Register } from './components/Register';
-import ListaLeads from './components/ListaLeads';
-import './index.css';
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <Register />
-        <ListaLeads />
-      </div>
-    </AuthProvider>
+    <div className="App">
+      <Register />
+    </div>
   );
 }
 
