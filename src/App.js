e11e93f3ff55c62a1a80ac383e0efe72fbc9da8b
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 11:24:42 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
-import { Register } from './components/Register';
+import { Register } from './components/Register'; // GAranta que NÃO está ReRegister
 import './index.css';
 
 function App() {
