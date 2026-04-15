8ad39194ab44e4296b7bd3c4e0a0f137a30ca847
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 17:09:42 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
 import { Register } from './components/Register';
+import ListaLeads from './components/ListaLeads';
 import './index.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <AuthProvider>
       <div className="App">
         <Register />
+        <ListaLeads />
       </div>
     </AuthProvider>
   );
