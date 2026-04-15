2a2aa02e7036d1a5e6ca82ecf34682a27d40252c
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 10:29:40 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
-import { Login } from './components/Login';
 import { Register } from './components/Register';
 import './index.css';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <AuthProvider>
       <div className="App">
-        {/* Ative apenas um por vez para testar */}
         <Register />
       </div>
     </AuthProvider>
