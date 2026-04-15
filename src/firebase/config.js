476f03369b1af173cb9baad1a63c4fde538fa0c2
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 10:26:08 -0300

    config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,20 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Credenciais conectadas ao projeto Klinni IA
 const firebaseConfig = {
-  apiKey: "AIzaSy...", 
+  apiKey: "AIzaSyCv7kNOOa1AT71TmvwKLdwi8TyHHVh6htM",
   authDomain: "klinni-ia.firebaseapp.com",
   projectId: "klinni-ia",
-  storageBucket: "klinni-ia.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  storageBucket: "klinni-ia.firebasestorage.app",
+  messagingSenderId: "761229946691",
+  appId: "1:761229946691:web:feeceb3caed42445be09f6"
 };
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta as funções para serem usadas no AuthContext.js e outros arquivos
+// Exporta os serviços garantindo que o 'app' foi passado como argumento
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
