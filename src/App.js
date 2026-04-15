219b04e5b0e72a8050201c5e9e97658adfa9cd43
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 09:55:40 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
 import { Login } from './components/Login';
+import { Register } from './components/Register'; 
 import './index.css';
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
-        {/* O Login agora está protegido pelo AuthProvider */}
-        <Login />
+        {/* Ative apenas um por vez para testar */}
+        <Register />
       </div>
     </AuthProvider>
   );
