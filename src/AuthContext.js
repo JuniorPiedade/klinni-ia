992c3bf4fb8ddd8919c2a990df345901c8f55a6d
JuniorPiedade <124215139+JuniorPiedade@users.noreply.github.com>
2026-04-15 12:44:29 -0300

    AuthContext.js

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -12,18 +12,15 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
 
   function signup(email, password) {
-    // Verificação extra para evitar o 'argument-error'
     if (!auth) return Promise.reject("Firebase Auth não inicializado");
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
   useEffect(() => {
-    // Se o config.js falhar por qualquer motivo, o app não vai quebrar
     if (!auth) {
       setLoading(false);
       return;
     }
-
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       setLoading(false);
